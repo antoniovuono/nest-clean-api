8a8d2a7ae3cf015f00119c6f3df5576f000b546c
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-07 22:20:41 -0300

    feature: fist controller created

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,37 @@
+import {
+  ConflictException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userAlreadyExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userAlreadyExists) {
+      throw new ConflictException('User with same email already registered')
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+  }
+}
