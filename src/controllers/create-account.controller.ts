2a241727a6a4a5d5d3c772fc54a3897b9a22ea70
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-08 21:03:57 -0300

    feature: error validation with zod and pipes

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,18 +4,30 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
 import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBody = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
+@UsePipes(new ZodValidationPipe(createAccountBodySchema))
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
-    const { name, email, password } = body
+  async handle(@Body() body: CreateAccountBody) {
+    const { name, email, password } = createAccountBodySchema.parse(body)
 
     const userAlreadyExists = await this.prisma.user.findUnique({
       where: {
